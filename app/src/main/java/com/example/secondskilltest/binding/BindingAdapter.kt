86c536fef2ee58c209e2e9275aaad8daff8a1a74
jangjh123 <gn000123@naver.com>
2022-10-14 12:13:01 +0900

    [feat] RecyclerView 를 위한 BindingAdapter 함수 추가

--- app/src/main/java/com/example/secondskilltest/binding/BindingAdapter.kt
+++ app/src/main/java/com/example/secondskilltest/binding/BindingAdapter.kt
@@ -1,4 +1,23 @@
 package com.example.secondskilltest.binding
 
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
 object BindingAdapter {
+    @JvmStatic
+    @BindingAdapter("setAdapter")
+    fun setAdapter(
+        view: RecyclerView,
+        adapter: ListAdapter<Any?, RecyclerView.ViewHolder>
+    ) {
+        view.adapter = adapter
+    }
+
+    @JvmStatic
+    @BindingAdapter("setLayoutManager")
+    fun setLayoutManager(view: RecyclerView, orientation: Int) {
+        view.layoutManager = LinearLayoutManager(view.context, orientation, false)
+    }
 }
